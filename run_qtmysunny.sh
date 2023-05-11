a7996878d3ded029385014332154a5040857cb13
qubo <346386694@qq.com>
2023-05-11 16:56:38 +0800

    first commit

--- run_qtmysunny.sh
+++ run_qtmysunny.sh
@@ -1,4 +1,9 @@
 #！/bin/bash
+mkdir DATA
+mkdir SAVE
 xhost +
-docker run --rm --network=host -itd --name qtmysunny --privileged -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=:0 qubo188612/ubuntu20.04:qtmysunny
+docker run --rm --network=host -itd --name qtmysunny \
+--mount type=bind,source=./SAVE,destination=/workspace/qtmysunny/SAVE \
+--mount type=bind,source=./DATA,destination=/workspace/qtmysunny/DATA \
+--privileged -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=:0 qubo188612/ubuntu20.04:qtmysunny
 
