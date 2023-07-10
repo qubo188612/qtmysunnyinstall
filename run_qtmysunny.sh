163c9f33f303c4f831a744b34672498b9fa4fef3
qubo188612 <346386694@qq.com>
2023-07-10 13:56:25 +0800

    first commit

--- run_qtmysunny.sh
+++ run_qtmysunny.sh
@@ -5,5 +5,5 @@ xhost +
 docker run --rm --network=host -itd --name qtmysunny \
 --mount type=bind,source=./SAVE,destination=/workspace/qtmysunny/SAVE \
 --mount type=bind,source=./DATA,destination=/workspace/qtmysunny/DATA \
---privileged -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=:0 qubo188612/ubuntu20.04:qtmysunny
+--privileged -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=:0 -v /path/to/host/home:/path/to/contatiner/home --env LANG=C.UTF-8 qubo188612/ubuntu20.04:qtmysunny
 
